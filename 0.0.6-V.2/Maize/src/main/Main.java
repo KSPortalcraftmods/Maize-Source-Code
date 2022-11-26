24637eaa47f5ba939616796183a9e24939aa6785
KSPortalcraftDevolpment <65189871+KSPortalcraftmods@users.noreply.github.com>
2022-11-25 19:57:04 -0500

    0.0.6
    Added back tile manager (thanks eclipse). One more version then should be on pair with pre wiped build.

---
+++ 0.0.6-V.2/Maize/src/main/Main.java
@@ -0,0 +1,31 @@
+//Last Edited 4:30pm 21/11/2022
+
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Maize: Indev, 0.0.6");
+		
+		
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("Maize");
+		
+		GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+		
+		window.pack();
+		// git cloning sucks 
+	    window.setLocationRelativeTo(null);
+	    window.setVisible(true);
+	    
+	    gamePanel.startGameThread();
+	}
+
+}
+  
